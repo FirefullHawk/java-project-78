6cda46170640fa12f0ddcb4437a609a8f0b4cf5c
Dmitry Simanov <szizni1@gmail.com>
2023-06-16 16:21:19 +0300

    fix linter trouble

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,16 +5,17 @@ import java.util.Objects;
 public class BaseSchema {
     private boolean requiredActive = false;
     /**
-     * This function
-     @return scheme-type and mark the required flag
+     * This function mark the required flag.
+     * @return scheme-type
      */
     public BaseSchema required() {
         requiredActive = true;
         return this;
     }
     /**
-     * This function check empty and null input
+     * This function check empty and null input.
      @return valid or not input object
+     @param inputData is Object which must be validated
      */
     public boolean isValid(Object inputData) {
         return !requiredActive && (Objects.equals(inputData, null) || Objects.equals(inputData, ""));
